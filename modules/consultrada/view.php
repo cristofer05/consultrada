62139b98bd88a6703f80dc7dc8814d0c24be8270
Kelvin Reyes <ingkelvinreyes@gmail.com>
2018-01-31 14:02:21 -0400

    otro

--- modules/consultrada/view.php
+++ modules/consultrada/view.php
@@ -33,10 +33,9 @@
                           <th class="column1">#</th>
                           <th class="column2">Foto</th>
                           <th class="column3">Barcode</th>
-                          <th class="column4">Qty</th>
                           <th class="column5">Nombre</th>
                           <th class="column6">Comentario</th>
-                          <th class="column7">Realizado</th>
+                          <th class="column7">Ubicacion</th>
                           <th class="column8">Accion</th>
                         </tr>
                       </thead>
@@ -45,90 +44,55 @@
                             <td class="column1">1</td>
                             <td class="column2"><spam class='nufoto'>105</spam></td>
                             <td class="column3">08456312541 GA</td>
-                            <td class="column4">8</td>
                             <td class="column5">RadioShack Cargador de laptop</td>
                             <td class="column6">Azul</td>
-                            <td class="column7"><input class='input-toggle' type='checkbox'></td>
+                            <td class="column7">A02</td>
                             <td class="column8">
                               <div>
-                                <a data-toggle='tooltip' data-placement='top' title='Ver en Amazon' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
-                                    <i style='color:#fff' class='glyphicon glyphicon-edit'>Amazon</i>
+                                <a data-toggle='tooltip' data-placement='top' title='Ver articulo' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
+                                    <i style='color:#fff' class='glyphicon glyphicon-eye-open'> VER</i>
                                 </a>
-                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Ver en eBay" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
-                                    <i style="color:#fff" class="glyphicon glyphicon-print">eBay</i>
+                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Dar Entrada" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
+                                    <i style="color:#fff" class="glyphicon glyphicon-edit"> ENTRADA</i>
                                 </a>
-
-                                <a data-toggle="tooltip" data-placement="top" title="Ver en Google" class="btn btn-danger btn-sm" href="https://www.google.com.do/search?q=<?php echo "888"; ?>">
-                                    <i style="color:#fff" class="glyphicon glyphicon-trash">Google</i>
                                 </a>
                               </div>
                             </td>
                           </tr>
                           <tr>
-                            <td class="column1">1</td>
-                            <td class="column2"><spam class='nufoto'>105</spam></td>
-                            <td class="column3">08456312541 GA</td>
-                            <td class="column4">8</td>
+                            <td class="column1">2</td>
+                            <td class="column2"><spam class='nufoto'>106</spam></td>
+                            <td class="column3">08456312541 GC B</td>
                             <td class="column5">RadioShack Cargador de laptop</td>
                             <td class="column6">Azul</td>
-                            <td class="column7"><input class='input-toggle' type='checkbox'></td>
+                            <td class="column7">B06</td>
                             <td class="column8">
                               <div>
-                                <a data-toggle='tooltip' data-placement='top' title='Ver en Amazon' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
-                                    <i style='color:#fff' class='glyphicon glyphicon-edit'>Amazon</i>
+                                <a data-toggle='tooltip' data-placement='top' title='Ver articulo' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
+                                    <i style='color:#fff' class='glyphicon glyphicon-eye-open'> VER</i>
                                 </a>
-                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Ver en eBay" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
-                                    <i style="color:#fff" class="glyphicon glyphicon-print">eBay</i>
+                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Dar Entrada" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
+                                    <i style="color:#fff" class="glyphicon glyphicon-edit"> ENTRADA</i>
                                 </a>
-
-                                <a data-toggle="tooltip" data-placement="top" title="Ver en Google" class="btn btn-danger btn-sm" href="https://www.google.com.do/search?q=<?php echo "888"; ?>">
-                                    <i style="color:#fff" class="glyphicon glyphicon-trash">Google</i>
                                 </a>
                               </div>
                             </td>
                           </tr>
                           <tr>
-                            <td class="column1">1</td>
-                            <td class="column2"><spam class='nufoto'>105</spam></td>
-                            <td class="column3">08456312541 GA</td>
-                            <td class="column4">8</td>
+                            <td class="column1">3</td>
+                            <td class="column2"><spam class='nufoto'>107</spam></td>
+                            <td class="column3">08456312541 GB</td>
                             <td class="column5">RadioShack Cargador de laptop</td>
                             <td class="column6">Azul</td>
-                            <td class="column7"><input class='input-toggle' type='checkbox'></td>
+                            <td class="column7">A24</td>
                             <td class="column8">
                               <div>
-                                <a data-toggle='tooltip' data-placement='top' title='Ver en Amazon' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
-                                    <i style='color:#fff' class='glyphicon glyphicon-edit'>Amazon</i>
+                                <a data-toggle='tooltip' data-placement='top' title='Ver articulo' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
+                                    <i style='color:#fff' class='glyphicon glyphicon-eye-open'> VER</i>
                                 </a>
-                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Ver en eBay" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
-                                    <i style="color:#fff" class="glyphicon glyphicon-print">eBay</i>
+                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Dar Entrada" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
+                                    <i style="color:#fff" class="glyphicon glyphicon-edit"> ENTRADA</i>
                                 </a>
-
-                                <a data-toggle="tooltip" data-placement="top" title="Ver en Google" class="btn btn-danger btn-sm" href="https://www.google.com.do/search?q=<?php echo "888"; ?>">
-                                    <i style="color:#fff" class="glyphicon glyphicon-trash">Google</i>
-                                </a>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="column1">1</td>
-                            <td class="column2"><spam class='nufoto'>105</spam></td>
-                            <td class="column3">08456312541 GA</td>
-                            <td class="column4">8</td>
-                            <td class="column5">RadioShack Cargador de laptop</td>
-                            <td class="column6">Azul</td>
-                            <td class="column7"><input class='input-toggle' type='checkbox'></td>
-                            <td class="column8">
-                              <div>
-                                <a data-toggle='tooltip' data-placement='top' title='Ver en Amazon' style='margin-right:5px' class='btn btn-primary btn-sm' href="https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=<?php echo "8888"; ?>">
-                                    <i style='color:#fff' class='glyphicon glyphicon-edit'>Amazon</i>
-                                </a>
-                               <a data-toggle="tooltip" data-placement="top" target="_blank" title="Ver en eBay" class="btn btn-primary btn-sm" href="https://www.ebay.com/sch/i.html?LH_BIN=1&_nkw=<?php echo "8888"; ?>&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684">
-                                    <i style="color:#fff" class="glyphicon glyphicon-print">eBay</i>
-                                </a>
-
-                                <a data-toggle="tooltip" data-placement="top" title="Ver en Google" class="btn btn-danger btn-sm" href="https://www.google.com.do/search?q=<?php echo "888"; ?>">
-                                    <i style="color:#fff" class="glyphicon glyphicon-trash">Google</i>
                                 </a>
                               </div>
                             </td>
@@ -143,31 +107,6 @@
 						<!-- FIN MENSAJE DE SUCESS PARA LISTA DE PRODUCTOS -->
 					</div><!-- Carga los datos ajax -->
 				</div>
-
-        <!-- Ejemplo de como se podrian mostrar los resultados -->
-				<div class="row">
-					<div class="outer_div">
-            <div class="col-xs-4">
-                <button class="btn btn-primary sku" type="button">
-                  75489562654896 GA MB
-                </button>
-              </div>
-              <div class="col-xs-4">
-                <button class="btn btn-primary sku" type="button">
-                  75489562654896 GA B
-                </button>
-              </div>
-              <div class="col-xs-4">
-                <button class="btn btn-primary sku" type="button">
-                  75489562654896 GA B
-                </button>
-              </div>
-              <div class="col-xs-4">
-                <button class="btn btn-primary sku" type="button">
-                  75489562654896 GA B
-                </button>
-              </div>
-			    </div>
           <!-- Carga los datos ajax -->
 				</div>
 			</form>
