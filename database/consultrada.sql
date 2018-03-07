9d9084b64b83a110b109ea58d776740fb2cf8ff4
cristofer05 <cristofel_b5@hotmail.com>
2018-03-07 19:02:50 -0400

    DB - ajustes de corte y contenido en tabla corte

--- database/consultrada.sql
+++ database/consultrada.sql
@@ -21,12 +21,12 @@ SET time_zone = "-04:00";
 --
 
 -- --------------------------------------------------------
-
+DROP TABLE IF EXISTS productos;
 --
 -- Estructura de tabla para la tabla `productos`
 --
 CREATE TABLE `productos` (
-  `id_producto` int(6) NOT NULL,
+  `id_producto` int(11) NOT NULL,
   `barcode` varchar(18) NOT NULL,
   `barcode_final` varchar(50) NOT NULL,
   `nombre_producto` varchar(100) NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE `productos` (
   `comentario` varchar(250) NOT NULL,
   `realizado` varchar(20) NOT NULL,
   `imagen` varchar(70) NOT NULL,
-  `id_corte` int(6) NOT NULL,
+  `id_corte` int(11) NOT NULL,
   `fecha_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `qty_total` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -47,12 +47,12 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `barcode`, `barcode_final`, `nombre_producto`, `condicion`, `missing`, `qty`, `ubicacion`, `nu_foto`, `comentario`, `realizado`, `imagen`, `id_corte`, `fecha_creacion`, `qty_total`) VALUES
-('1', '888182998397', '888182998397 GA B','HP 920XL Magenta High Yield Original Ink Cartridge (CD973AN)', 'GA', 'B', 5, 'B05','101', 'N/A', 'NO', 'notebook.jpg', '0', '2018-01-25 18:43:20',5),
-('2', '013803238310', '013803238310 O M','Black & Decker Lithium Hand Vacuum Lightweight Portable (Red)', 'O', 'M', 2, 'A02','102', 'N/A', 'SI', 'playstation.jpg', '0', '2018-01-25 18:43:20',10),
-('3', '884420736783', '884420736783','AT&T Prepaid - Bring Your Own Phone or Tablet SIM Card (AT&T)', 'NEW', '-', 1, 'B25','103', 'N/A', 'NO', 'iphone.jpg', '0', '2018-01-25 18:43:20',25);
+('1', '888182998397', '888182998397 GA B','HP 920XL Magenta High Yield Original Ink Cartridge (CD973AN)', 'GA', 'B', 5, 'B05','101', 'N/A', 'NO', 'notebook.jpg', 1, '2018-01-25 18:43:20',5),
+('2', '013803238310', '013803238310 O M','Black & Decker Lithium Hand Vacuum Lightweight Portable (Red)', 'O', 'M', 2, 'A02','102', 'N/A', 'SI', 'playstation.jpg', 1, '2018-01-25 18:43:20',10),
+('3', '884420736783', '884420736783','AT&T Prepaid - Bring Your Own Phone or Tablet SIM Card (AT&T)', 'NEW', '-', 1, 'B25','103', 'N/A', 'NO', 'iphone.jpg', 1, '2018-01-25 18:43:20',25);
 
 -- --------------------------------------------------------
-
+DROP TABLE IF EXISTS socios;
 --
 -- Estructura de tabla para la tabla `socios`
 --
@@ -88,7 +88,7 @@ INSERT INTO `socios` (`codigo`, `nombres`, `apellidos`, `cedula`, `fnacimiento`,
 ('CAP-000364', 'Maria Rosa', 'Rodriguez Almonte', '001114343541', '1987-01-12', 'Femenino', 'Santo Domingo', 'Estudiante', 'maria@gmail.com', '849-643-2151', '829-623-3358','Standar', '2018-01-26', 1, '2017-07-24 16:43:20', '2017-01-02', 1, '2017-07-26 02:09:06');
 
 -- --------------------------------------------------------
-
+DROP TABLE IF EXISTS logs;
 --
 -- Estructura de tabla para la tabla `logs`
 --
@@ -119,6 +119,7 @@ INSERT INTO `logs` (`id_log`, `id_producto`, `id_user`, `fecha_log`, `registro`,
 (6, 2, 1, '2018-02-05 17:09:06', 'Articulo creado', 0,'titulo','A40',3);
 
 -- --------------------------------------------------------
+DROP TABLE IF EXISTS cortes;
 --
 -- Estructura de tabla para la tabla `cortes`
 --
@@ -127,10 +128,18 @@ CREATE TABLE `cortes` (
   `id_corte` int(11) NOT NULL,
   `nombre_corte` varchar(100) NOT NULL,
   `fecha` varchar(50) NOT NULL,
-  `qty_productos` int(6) NOT NULL
+  `num_productos` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `cortes`
+--
 
+INSERT INTO `cortes` (`id_corte`, `nombre_corte`, `fecha`, `num_productos`) VALUES
+(1, 'Corte #0001', '2018-01-25 17:09:06', 52);
+
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS usuarios;
 --
 -- Estructura de tabla para la tabla `usuarios`
 --
@@ -167,7 +176,8 @@ INSERT INTO `usuarios` (`id_user`, `username`, `name_user`, `password`, `email`,
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`),
-  ADD UNIQUE KEY `barcode_final` (`barcode_final`);
+  ADD UNIQUE KEY `barcode_final` (`barcode_final`),
+  ADD KEY `id_corte` (`id_corte`);
 
 --
 -- Indices de la tabla `socios`
@@ -246,6 +256,12 @@ ALTER TABLE `socios`
 ALTER TABLE `logs`
   ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON UPDATE CASCADE,
   ADD CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `usuarios` (`id_user`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `logs`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_corte`) REFERENCES `cortes` (`id_corte`) ON UPDATE CASCADE;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
