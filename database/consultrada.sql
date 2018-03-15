987a27587f54c2b45982573ceee790fca03d0666
cristofer05 <cristofel_b5@hotmail.com>
2018-03-15 15:39:54 -0400

    Ajustes en base de datos

--- database/consultrada.sql
+++ database/consultrada.sql
@@ -154,7 +154,7 @@ CREATE TABLE `cortes` (
 --
 
 INSERT INTO `cortes` (`id_corte`, `nombre_corte`, `fecha`, `num_productos`, `estado`) VALUES
-(1, 'Corte #0001', '2018-01-25 17:09:06', 52, 'actual');
+(1, 'Corte #0001', '2018-01-25 17:09:06', 0, 'actual');
 
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS usuarios;
